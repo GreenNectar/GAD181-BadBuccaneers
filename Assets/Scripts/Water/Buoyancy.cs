968a2619e6631bae9d9582d0250e511ae734ac50
Green <nitricgreennectar@gmail.com>
2021-07-16 00:19:49 +0800

    Buoyancy Drag Update
    Water level finder no longer necessary when using Buoyancy
    When no water level finder is attached to buoyancy it will zero as the height
    Added tooltips to the Buoyancy script
    Buoyancy now allows for directional draglessness? Now you can have a boat cut through water without drag!!! (or lower drag too!)

--- Assets/Scripts/Water/Buoyancy.cs
+++ Assets/Scripts/Water/Buoyancy.cs
@@ -4,32 +4,48 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-[RequireComponent(typeof(WaterLevelFinder))]
 [RequireComponent(typeof(Rigidbody))]
 public class Buoyancy : MonoBehaviour
 {
-    [SerializeField, Range(0, 20)]
+    [SerializeField, Range(0, 20), Tooltip("How much drag the object has when under the water")]
     private float drag = 1f;
-    [SerializeField, Range(0, 100)]
+    [SerializeField, Range(0, 100), Tooltip("How buoyant the object is")]
     private float buoyancy = 10f;
-    [SerializeField]
+    [SerializeField, Tooltip("How heigh above the water surface you want the buoyancy to rise to")]
     private float heightOffset = 0f;
 
-    [SerializeField] private bool realtime;
-    [SerializeField] private bool usesNormal;
+    [SerializeField, Tooltip("Uses physics updates or normal updates")]
+    private bool realtime;
+    [SerializeField, Tooltip("Whether or not the water can push you in the direction of the wave normal")]
+    private bool usesNormal;
 
-    [SerializeField, ShowIf("usesNormal"), Range(0f, 1f)]
+    [SerializeField, Tooltip("This will determine which direction the object can move without drag\n(length 0 = full drag, 1 = no drag in that direction)\n(local space)")]
+    private Vector3 draglessDirection = Vector3.zero;
+    //[SerializeField, Range(0f, 1f), Tooltip("The range of the drag that is dragless...")]
+    //private float draglessWidth = 1f;
+    [SerializeField, Tooltip("Whether or not the dragless direction applies to the opposite direction as well")]
+    private bool draglessOneDirection = true;
+
+    [SerializeField, ShowIf("usesNormal"), Range(0f, 1f), Tooltip("How much of the normal affects the object (0-1)")]
     private float normalStrength = 1f;
 
-    [SerializeField]
+    [SerializeField, Tooltip("The positions on which the bouyancy will be applied to on the object")]
     private Vector3[] positions = new Vector3[1];
-    Vector3[] prevPositions;
+    Vector3[] prevPositions; // To keep track of velocities
 
-    [SerializeField]
+    [SerializeField, Tooltip("Allows the gizmos to be seen")]
     private bool drawGizmos;
+    [SerializeField, ShowIf("drawGizmos"), Tooltip("The size of the gizmos")]
     private float gizmoSize = 0.1f;
 
+    /// <summary>
+    /// How many points are submersed underneath the water
+    /// </summary>
+    [HideInInspector]
     public int submersed = 0;
+    /// <summary>
+    /// Percentage of points submersed in the water
+    /// </summary>
     public float SubmersedPercentage
     {
         get
@@ -72,27 +88,39 @@ public class Buoyancy : MonoBehaviour
         {
             Vector3 position = transform.TransformPoint(positions[i]);
             Vector3 velocity = (position - prevPositions[i]) / Time.deltaTime;
-            Vector3 waterPosition = finder.GetWaterSurfacePosition(position) + Vector3.up * heightOffset;
+            Vector3 dragVelocity = velocity;
 
-            Vector3 normal = Vector3.up;
-
-            if (usesNormal)
+            if (draglessDirection.sqrMagnitude > 0f)
             {
-                //normal = finder.GetWaterNormal(position);
-                normal = (finder.GetWaterNormal(position) * normalStrength) + (Vector3.up * (1f - normalStrength));
-                //if (normal == Quaternion.FromToRotation(Vector3.zero, normal) * Vector3.forward)
-                //{
-                //    Debug.Log("Bruh");
-                //}
+                float amountOfDrag = Vector3.Dot(-dragVelocity.normalized, transform.TransformDirection(draglessDirection).normalized);
+                //Debug.Log($"Dot product - {amountOfDrag}");
+                amountOfDrag = draglessOneDirection ? Mathf.Clamp(amountOfDrag, 0f, 1f) : Mathf.Abs(amountOfDrag);
+                //Debug.Log($"Drag 0f to 1f - {amountOfDrag}");
+                dragVelocity = dragVelocity * (1f - (amountOfDrag * Mathf.Clamp(draglessDirection.magnitude, 0f, 1f)));
             }
 
+            // Get the water position
+            Vector3 waterPosition = finder != null ? finder.GetWaterSurfacePosition(position) + Vector3.up * heightOffset : new Vector3(position.x, heightOffset, position.z);
+
+            // Normal vector of the point
+            Vector3 normal = Vector3.up;
+            if (usesNormal)
+            {
+                normal = ((finder.GetWaterNormal(position) * normalStrength) + (Vector3.up * (1f - normalStrength))).normalized;
+            }
+
+            // Difference of where we are to where we want to be
             float difference = position.y - waterPosition.y;
             if (difference < 0)
             {
                 submersed++;
-                difference = Mathf.Clamp(-difference, 0f, 3f);
+                difference = Mathf.Clamp(-difference, 0f, 3f); // clamp the difference, I can't remember why this is...
 
-                rb.AddForceAtPosition(((drag * rb.mass) * -velocity / positions.Length) * Time.deltaTime * 60f, position);
+                // Drag force
+                rb.AddForceAtPosition(((drag * rb.mass) * -dragVelocity / positions.Length) * Time.deltaTime * 60f, position);
+                //rb.AddForceAtPosition(((drag * rb.mass) * -velocity / positions.Length) * Time.deltaTime * 60f, position);
+
+                // Bouyancy force
                 rb.AddForceAtPosition(((buoyancy * rb.mass) * normal * difference / positions.Length) * Time.deltaTime * 60f, position);
             }
 
@@ -123,7 +151,16 @@ public class Buoyancy : MonoBehaviour
                 Gizmos.DrawSphere(transform.TransformPoint(position), gizmoSize);
 
                 Gizmos.color = Color.green;
-                Gizmos.DrawSphere(finder.GetWaterSurfacePosition(transform.TransformPoint(position)) + Vector3.up * heightOffset, gizmoSize);
+                if (finder)
+                {
+                    Gizmos.DrawSphere(finder.GetWaterSurfacePosition(transform.TransformPoint(position)) + Vector3.up * heightOffset, gizmoSize);
+                }
+                else
+                {
+                    Vector3 pos = transform.TransformPoint(position);
+                    pos.y = heightOffset;
+                    Gizmos.DrawSphere(pos, gizmoSize);
+                }
             }
 
             //Gizmos.DrawSphere(finder.GetWaterSurfacePosition(transform.position), 0.1f);
