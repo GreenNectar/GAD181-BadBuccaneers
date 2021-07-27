69f4591edefdbd2cb2157f6dc9ffb74be2e35a18
Mitchell Heath <mitchellheath2001@gmail.com>
2021-07-27 13:05:45 +0800

    Boat Race Prototype Done
    Started Cave Run UwU

--- Assets/Scripts/Mitch/CaveRaseMovement.cs
+++ Assets/Scripts/Mitch/CaveRaseMovement.cs
@@ -5,52 +5,95 @@ using Rewired;
 
 public class CaveRaseMovement : MicroGamePlayerController
 {
+    CharacterController characterController;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+    public Animator anim;
+    public bool dead = false;
     public float fowardSpeed = 1f;
-    public float moveSpeed = 1f;
-    public Transform jumpSpot;
-    public Transform duckSpot;
-    public Transform defultSpot;
 
+    private Vector3 moveDirection = Vector3.zero;
+
+    private float startingHeight;
+    private float previousHeight;
     [SerializeField]
-    private bool isMoving;
-    private Vector3 desiredPos;
-   
+    bool delay;
+
     protected override void Start()
     {
         base.Start();
+        characterController = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
+        startingHeight = characterController.height;
+        delay = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * fowardSpeed);
-
-        if(!isMoving && player.GetButtonDown("Jump"))
+        characterController.Move(moveDirection * Time.deltaTime);
+        moveDirection.y -= gravity * Time.deltaTime;
+        if(dead == false)
         {
-            //desiredPos = jumpSpot.position;
-            desiredPos = new Vector3(transform.position.x, transform.position.y, jumpSpot.position.z);
-            isMoving = true;
+            characterController.Move(Vector3.forward * Time.deltaTime * fowardSpeed);
         }
 
-        if(!isMoving && player.GetButtonDown("Duck"))
+        if (player.GetButtonDown("Jump") && delay == false && dead == false)
         {
-            desiredPos = duckSpot.position;
-            isMoving = true;
-        }
-
-        if(isMoving)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, desiredPos, moveSpeed * Time.deltaTime);
-
-            if(transform.position == desiredPos)
+            if (characterController.velocity.y == 0)
             {
-                isMoving = false;
-                transform.position = desiredPos;
+                moveDirection.y = jumpSpeed;
+                anim.SetTrigger("Jumped");
+                Debug.Log("Jumping");
+                delay = true;
+                StartCoroutine(DelayAction());
             }
+           // moveDirection.y = jumpSpeed;
+            Debug.Log("Pressed Jump");
         }
-        else
+
+        if(player.GetButtonDown("Duck") && delay == false && dead == false)
         {
-            transform.position = Vector3.MoveTowards(transform.position, defultSpot.position, moveSpeed * Time.deltaTime);
+            anim.SetTrigger("Ducked");
+            delay = true;
+            StartCoroutine(DelayAction());
         }
+
+        // Change the capsule's height to slide
+        float characterRadius = characterController.radius;
+        float newHeight = anim.GetBool("IsDucked") ? characterRadius : startingHeight;
+        characterController.height = newHeight;
+
+        // Keep the bottom of the collider on the ground
+        if (newHeight != previousHeight)
+        {
+            characterController.Move(Vector3.down * characterController.radius * (newHeight == characterRadius ? 1f : -1f));
+            previousHeight = newHeight;
+        }
+
+        // You might want to have a custom camera follow script that follows the x and z but not y
+        // The reason the camera flicks up and down is because the origin moves up and down
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Trip"))
+        {
+            anim.SetTrigger("Tripped");
+            dead = true;
+        }
+        
+        if (other.CompareTag("HitAbove"))
+        {
+            anim.SetTrigger("Died");
+            dead = true;
+        }
+    }
+
+    IEnumerator DelayAction()
+    {
+        yield return new WaitForSeconds(0.9f);
+        delay = false;
     }
 }
