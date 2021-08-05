03edeac629f0d6ac906d39721444d8bb8529320f
Tom <1024072@student.sae.edu.au>
2021-08-05 14:30:11 +0800

    CanyonClimber
    Scene finished
    Camera need work
    animation need work

--- Assets/Scripts/CanyonClimbScripts/CameraMovingUpwards.cs
+++ Assets/Scripts/CanyonClimbScripts/CameraMovingUpwards.cs
@@ -9,16 +9,11 @@ public class CameraMovingUpwards : MonoBehaviour
     public float camSpeed;
     private Vector3 Vec3 = Vector3.zero;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
     // Update is called once per frame
     void Update()
     {
         cam.Move(Vec3 * Time.deltaTime);
         cam.Move(Vector3.up * Time.deltaTime * camSpeed);
+
     }
 }
