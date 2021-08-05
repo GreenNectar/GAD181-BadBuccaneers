0d1ed37eb594b23a6c1901c5e639ef4ffb9695c0
Tom <1024072@student.sae.edu.au>
2021-08-05 13:18:43 +0800

    Auto stash before checking out "main"

--- Assets/Scripts/CanyonClimbScripts/PlayerCanyonClimber.cs
+++ Assets/Scripts/CanyonClimbScripts/PlayerCanyonClimber.cs
@@ -22,6 +22,7 @@ public class PlayerCanyonClimber : MicroGamePlayerController
 
 
     private Vector3 velocity;
+    private Vector3 startingPosition;
 
     protected override void Start()
     {
@@ -32,9 +33,11 @@ public class PlayerCanyonClimber : MicroGamePlayerController
     // Update is called once per frame
     void Update()
     {
+        var move = new Vector3(player.GetAxis("LeftMoveX"), 0f, 0f);
         move = new Vector3(player.GetAxis("LeftMoveX"), 0f, 0f);
         controller.Move(move * moveSpeed * Time.deltaTime);
         velocity += Physics.gravity * Time.deltaTime * gravMultiplier;
+        CollisionFlags collisionFlags = controller.Move(velocity * Time.deltaTime);
         CollisionFlags flags = controller.Move(velocity * Time.deltaTime);
 
         if (controller.isGrounded)
@@ -44,20 +47,39 @@ public class PlayerCanyonClimber : MicroGamePlayerController
                 velocity = -Physics.gravity.normalized * jumpVelocity;
             }
         }
+        else if (collisionFlags == CollisionFlags.Above)
 
         if (flags == CollisionFlags.Above)
         {
+            velocity = Physics.gravity * Time.deltaTime;
             velocity = Physics.gravity * Time.deltaTime * gravMultiplier;
         }
 
+        /* 
+         if (move > 0 && !facingRight)
+         {
+             FlipCharacter();
+         }
         controller.enabled = false;
         Vector3 temp = transform.position;
         transform.position = new Vector3(temp.x, temp.y, startingPosition.z);
         controller.enabled = true;
 
+         else if (move < 0 && facingRight)
+         {
+             FlipCharacter();
+         }
+        */
+
+        /* if (Input.GetButtonDown("Jump"))
+         {
+             isJumping = true;
+         }
+        */
         SetCharacterRotation();
     }
 
+        //isGrounded = Physics.OverlapSphere(groundCheck.position, checkRadius, groundObjects);
     private void SetCharacterRotation()
     {
         Vector3 wantedMove = move;
@@ -66,11 +88,28 @@ public class PlayerCanyonClimber : MicroGamePlayerController
             wantedMove = Vector3.back;
         }
 
+        // This clamps the flag on the y axis
+        controller.enabled = false;
+        Vector3 temp = transform.position;
+        transform.position = new Vector3(temp.x, temp.y, startingPosition.z);
+        controller.enabled = true;
+    }
         Quaternion wantedRotation = Quaternion.LookRotation(wantedMove, Vector3.up);
         Vector3 wantedEuler = wantedRotation.eulerAngles;
         wantedEuler.x = 0f;
         wantedEuler.z = 0f;
         transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(wantedEuler), Time.deltaTime * rotationSpeed);
+
+
+    /*
+     private void FlipCharacter()
+     {
+         facingRight = !facingRight;
+         transform.Rotate(0f, 180f, 0f);
+     }
+    */
+
+    
 
     }
 }
