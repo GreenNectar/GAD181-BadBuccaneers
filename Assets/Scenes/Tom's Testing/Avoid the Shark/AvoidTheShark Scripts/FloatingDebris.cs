54a9c9a4b36bd8e28a9b90839569c5125929f134
Tom <1024072@student.sae.edu.au>
2021-08-11 21:56:38 +0800

    Avoid the Shark
    Adjusted scene for player to face neutral position for rotation script
    Adjusted debris spawn time and location to be more varied locations and more often. Mitch finding the game too hard
    Player movement not working on z axis

--- Assets/Scenes/Tom's Testing/Avoid the Shark/AvoidTheShark Scripts/FloatingDebris.cs
+++ Assets/Scenes/Tom's Testing/Avoid the Shark/AvoidTheShark Scripts/FloatingDebris.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class FloatingDebris : MonoBehaviour
 {
     public float downSpeed = 2f;
+    public float destroyTime = 5f;
 
+
+    
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.forward * Time.deltaTime * downSpeed);
+        Destroy(gameObject, destroyTime);
     }
+
+    
 }
