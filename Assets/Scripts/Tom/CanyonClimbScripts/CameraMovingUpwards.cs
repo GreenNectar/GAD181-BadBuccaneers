0a05c6d53b25647d1b92a87d24a610855f6da3f3
Tom <1024072@student.sae.edu.au>
2021-08-12 22:42:32 +0800

    CanyonClimb
    moved folder to tom script folder
    created 4 player aesthetic and locations. more booty at top of canyon
    CAMERA script delay time added

--- Assets/Scripts/Tom/CanyonClimbScripts/CameraMovingUpwards.cs
+++ Assets/Scripts/Tom/CanyonClimbScripts/CameraMovingUpwards.cs
@@ -8,16 +8,27 @@ public class CameraMovingUpwards : MonoBehaviour
     public float upSpeed = 2f;
     public float topHeight = 50f;
 
+    public float time = 3f;
+
+
+
 
     // Update is called once per frame
     void Update()
     {
+        if (time >= 0)
+        {
+            time -= Time.deltaTime;
+            return;
+        }
+        else
+        {
         transform.Translate(Vector3.up * Time.deltaTime * upSpeed);
-
         Vector3 clampedPosition = transform.position;
         clampedPosition.y = Mathf.Clamp(clampedPosition.y, 0f, topHeight);
         transform.position = clampedPosition;
+        }
+        
     }
-
-    
+ 
 }
