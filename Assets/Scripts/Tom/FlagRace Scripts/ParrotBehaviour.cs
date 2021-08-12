ae57c7ca92b9aaec53a07b27dbea4497ea91b724
Tom <1024072@student.sae.edu.au>
2021-08-12 23:17:17 +0800

    FlagRace change name and location

---
+++ Assets/Scripts/Tom/FlagRace Scripts/ParrotBehaviour.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParrotBehaviour : MonoBehaviour
+{
+
+    public int speed = 5;
+    public float knockback;
+    public float knockbackTime;
+  
+    // Update is called once per frame
+    // Moves the parrots across the screen smoothly
+    void Update()
+    {
+        transform.position += transform.forward * Time.deltaTime * speed;
+    }
+
+    // When a parrot collides with the player/flag, knocks them down the pole
+   void OnCollisionEnter(Collision collision)
+    {
+
+        Debug.Log("Collison");
+        // Check if collision is with Player
+        if (collision.gameObject.tag == "Player")
+        {
+
+            //collision.gameObject.transform.position -= transform.up * knockback;
+           
+                //When this object collides with Player, check if player has component (FlagMovement)
+                if (collision.gameObject.GetComponent<FlagMovement>() == true)
+                {
+                    //Flag movement takes knockback Time 
+                    collision.gameObject.GetComponent<FlagMovement>().TakeDamage(knockbackTime);
+                }
+
+
+        }
+
+    }
+   
+}
