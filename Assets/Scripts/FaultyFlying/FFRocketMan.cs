904045ee3c1a724bc2f6173e0278cde4caebc311
Rowan Koch <1022578@student.sae.edu.au>
2021-08-25 15:59:43 +0800

    FF Useless scripts, Clouds added, Animation Tests
    Started on some scripts, not sure whether they'll be implemented because smooth brain programmer am I.
    Clouds added for scene niceness. Shadows less harsh. 
    Animations added in but not properly implemented.

---
+++ Assets/Scripts/FaultyFlying/FFRocketMan.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FFRocketMan : MonoBehaviour
+{
+
+    public GameObject runningPirate; 
+
+    // Update is called once per frame
+    void Update()
+    {
+        //GameObject flyingPirateCopy = Instantiate(runningPirate //pointofcollision) as GameObject;
+        //Destroy(flyingPirateCopy);
+    }
+
+
+
+}
